25466ee76fdf1cb5170658994f9962a2fad2f85e
enzosdev <enzohfs@gmail.com>
2025-11-09 16:37:22 -0300

    build: setting up PostgresSQL connection and flyway miggration in application

---
+++ src/test/java/enzosdev/eventclean/EnventCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package enzosdev.eventclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnventCleanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
