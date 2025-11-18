48aa97af6c5ea252ed469ed0a91070b6472fadf8
enzosdev <enzohfs@gmail.com>
2025-11-18 18:54:59 -0300

    feat: EventEntity created and build an initial migration

--- src/main/java/enzosdev/eventclean/infrastructure/persistence/EventEntity.java
+++ src/main/java/enzosdev/eventclean/infrastructure/persistence/EventEntity.java
@@ -1,4 +1,30 @@
 package enzosdev.eventclean.infrastructure.persistence;
+import enzosdev.eventclean.core.enums.EventType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "Events")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class EventEntity {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    private String name;
+    private String description;
+    private String ticketIdentifier;
+    private LocalDateTime beginAt;
+    private LocalDateTime endAt;
+    private Integer capacity;
+    private String location;
+    private String eventOrganizer;
+    @Enumerated(EnumType.STRING)
+    private EventType type;
 }
