d0fb5541a93d4bfdfe7c38c263de16cf379fb2ec
enzosdev <enzohfs@gmail.com>
2025-11-18 18:47:34 -0300

    refactor: change capacity attribute to INT

--- src/main/java/enzosdev/eventclean/core/entities/Event.java
+++ src/main/java/enzosdev/eventclean/core/entities/Event.java
@@ -7,7 +7,7 @@ import java.util.Date;
 public record Event(Long id, String name,
                     String description, String ticketIdentifier,
                     Date beginAt, Date endAt,
-                    Long capacity, String location, String eventOrganizer,
+                    Integer capacity, String location, String eventOrganizer,
                     EventType type) {
 
 }
