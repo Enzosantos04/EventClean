4f8763fcaa1d0841ba484f1a99e7e2914ad3e9cc
enzosdev <enzohfs@gmail.com>
2025-11-18 18:19:21 -0300

    feat: Add migration V1__create_table_events

---
+++ src/main/java/enzosdev/eventclean/infrastructure/persistence/EventEntity.java
@@ -0,0 +1,4 @@
+package enzosdev.eventclean.infrastructure.persistence;
+
+public class EventEntity {
+}
---
+++ src/main/resources/db/migration/V1__Create_table_event.sql
@@ -0,0 +1,13 @@
+--V1 create event table
+CREATE TABLE events (
+ id SERIAL PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+description TEXT,
+ticket_identifier VARCHAR(255) NOT NULL UNIQUE,
+begin_at TIMESTAMP NOT NULL,
+end_at TIMESTAMP NOT NULL,
+capacity INTEGER,
+location VARCHAR(255) NOT NULL,
+event_organizer VARCHAR(255),
+event_type VARCHAR(100)
+);
