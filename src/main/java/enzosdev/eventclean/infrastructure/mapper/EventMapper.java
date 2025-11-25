28c9f95af48ceec400466ad1667ffb89bfefb04b
enzosdev <enzohfs@gmail.com>
2025-11-25 19:16:51 -0300

    refactor: change mapper structure

--- src/main/java/enzosdev/eventclean/infrastructure/mapper/EventMapper.java
+++ src/main/java/enzosdev/eventclean/infrastructure/mapper/EventMapper.java
@@ -1,44 +1,42 @@
 package enzosdev.eventclean.infrastructure.mapper;
 
 
+import enzosdev.eventclean.core.entities.Event;
 import enzosdev.eventclean.infrastructure.dtos.EventDTO;
-import enzosdev.eventclean.infrastructure.persistence.EventEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EventMapper {
 
-    public EventEntity map(EventDTO dto){
-        EventEntity eventEntity = new EventEntity();
-        eventEntity.setId(dto.getId());
-        eventEntity.setName(dto.getName());
-        eventEntity.setDescription(dto.getDescription());
-        eventEntity.setTicketIdentifier(dto.getTicketIdentifier());
-        eventEntity.setBeginAt(dto.getBeginAt());
-        eventEntity.setEndAt(dto.getEndAt());
-        eventEntity.setCapacity(dto.getCapacity());
-        eventEntity.setLocation(dto.getLocation());
-        eventEntity.setEventOrganizer(dto.getEventOrganizer());
-        eventEntity.setType(dto.getType());
-
-        return eventEntity;
+    public EventDTO toDto(Event event){
+        return  new EventDTO(
+                event.id(),
+                event.name(),
+                event.description(),
+                event.ticketIdentifier(),
+                event.beginAt(),
+                event.endAt(),
+                event.capacity(),
+                event.location(),
+                event.eventOrganizer(),
+                event.type()
+        );
     }
 
 
-    public EventDTO map(EventEntity event){
-        EventDTO eventDTO = new EventDTO();
-        eventDTO.setId(event.getId());
-        eventDTO.setName(event.getName());
-        eventDTO.setDescription(event.getDescription());
-        eventDTO.setTicketIdentifier(event.getTicketIdentifier());
-        eventDTO.setBeginAt(event.getBeginAt());
-        eventDTO.setEndAt(event.getEndAt());
-        eventDTO.setCapacity(event.getCapacity());
-        eventDTO.setLocation(event.getLocation());
-        eventDTO.setEventOrganizer(event.getEventOrganizer());
-        eventDTO.setType(event.getType());
-
-        return eventDTO;
-    }
+  public Event toEntity(EventDTO eventDTO){
+        return new Event(
+                eventDTO.id(),
+                eventDTO.name(),
+                eventDTO.description(),
+                eventDTO.ticketIdentifier(),
+                eventDTO.beginAt(),
+                eventDTO.endAt(),
+                eventDTO.capacity(),
+                eventDTO.location(),
+                eventDTO.eventOrganizer(),
+                eventDTO.type()
+        );
+  }
 
 }
