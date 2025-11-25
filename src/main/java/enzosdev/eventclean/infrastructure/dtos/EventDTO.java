32ed32ac33b05bd681f6ce367fba06cd620e45f1
enzosdev <enzohfs@gmail.com>
2025-11-25 11:59:53 -0300

    feat: DTO for  Event Entity

--- src/main/java/enzosdev/eventclean/infrastructure/dtos/EventDTO.java
+++ src/main/java/enzosdev/eventclean/infrastructure/dtos/EventDTO.java
@@ -1,4 +1,24 @@
 package enzosdev.eventclean.infrastructure.dtos;
 
+import enzosdev.eventclean.core.enums.EventType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EventDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private String ticketIdentifier;
+    private LocalDateTime beginAt;
+    private LocalDateTime endAt;
+    private Integer capacity;
+    private String location;
+    private String eventOrganizer;
+    private EventType type;
+
 }
