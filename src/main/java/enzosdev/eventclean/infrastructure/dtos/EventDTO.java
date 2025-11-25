df1195e8ebad98250970c538e437b3b5e2e3d2ea
enzosdev <enzohfs@gmail.com>
2025-11-25 19:15:57 -0300

    style: remove whitespace

--- src/main/java/enzosdev/eventclean/infrastructure/dtos/EventDTO.java
+++ src/main/java/enzosdev/eventclean/infrastructure/dtos/EventDTO.java
@@ -1,10 +1,6 @@
 package enzosdev.eventclean.infrastructure.dtos;
 
 import enzosdev.eventclean.core.enums.EventType;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import java.time.LocalDateTime;
 import java.util.Date;
 
 
