d9a94a96f45393848a6374436bfce4c1c7be1bda
enzosdev <enzohfs@gmail.com>
2025-11-25 20:28:48 -0300

    feat: EventRepositoryGateway (infrastructure) implements EventGateway (Core)

--- src/main/java/enzosdev/eventclean/infrastructure/gateway/EventRepositoryGateway.java
+++ src/main/java/enzosdev/eventclean/infrastructure/gateway/EventRepositoryGateway.java
@@ -1,4 +1,6 @@
 package enzosdev.eventclean.infrastructure.gateway;
 
-public class EventRepositoryGateway {
+import enzosdev.eventclean.core.gateway.EventGateway;
+
+public class EventRepositoryGateway implements EventGateway {
 }
