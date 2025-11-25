92b270f9051db4edba569692d81fba8e1829ec94
enzosdev <enzohfs@gmail.com>
2025-11-25 11:42:56 -0300

    feat: controller mock for event

--- src/main/java/enzosdev/eventclean/infrastructure/presentation/EventController.java
+++ src/main/java/enzosdev/eventclean/infrastructure/presentation/EventController.java
@@ -1,4 +1,17 @@
 package enzosdev.eventclean.infrastructure.presentation;
 
-public class controller {
+import enzosdev.eventclean.core.entities.Event;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class EventController {
+
+    @PostMapping("")
+    public String createEvent(@RequestBody Event event){
+        return "Event Created";
+    }
 }
