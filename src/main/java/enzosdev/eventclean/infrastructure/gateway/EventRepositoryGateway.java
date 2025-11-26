32640333ae4c7f927052504207906c472304071d
enzosdev <enzohfs@gmail.com>
2025-11-26 16:15:14 -0300

    feat: implements the interface EventGateway(core) and its method and have EventRepository as injection dependecy

--- src/main/java/enzosdev/eventclean/infrastructure/gateway/EventRepositoryGateway.java
+++ src/main/java/enzosdev/eventclean/infrastructure/gateway/EventRepositoryGateway.java
@@ -1,6 +1,22 @@
 package enzosdev.eventclean.infrastructure.gateway;
 
+import enzosdev.eventclean.core.entities.Event;
 import enzosdev.eventclean.core.gateway.EventGateway;
+import enzosdev.eventclean.infrastructure.mapper.EventMapper;
+import enzosdev.eventclean.infrastructure.persistence.EventRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EventRepositoryGateway implements EventGateway {
+
+    private final EventRepository eventRepository;
+
+    public EventRepositoryGateway(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @Override
+    public Event createEvent(Event event) {
+        return eventRepository.save(event);
+    }
 }
