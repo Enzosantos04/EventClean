a6fe6a82877a2f8ba75c18c4991089a69afc4e23
enzosdev <enzohfs@gmail.com>
2025-11-26 19:33:57 -0300

    feat: bean created to make context to controller

--- src/main/java/enzosdev/eventclean/infrastructure/beans/BeanConfiguration.java
+++ src/main/java/enzosdev/eventclean/infrastructure/beans/BeanConfiguration.java
@@ -1,4 +1,17 @@
 package enzosdev.eventclean.infrastructure.beans;
 
+import enzosdev.eventclean.core.gateway.EventGateway;
+import enzosdev.eventclean.core.usecases.CreateEventCase;
+import enzosdev.eventclean.core.usecases.CreateEventCaseImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class BeanConfiguration {
+
+    @Bean
+    public CreateEventCase createEventCase(EventGateway eventGateway){
+        return new CreateEventCaseImpl(eventGateway);
+
+    }
 }
