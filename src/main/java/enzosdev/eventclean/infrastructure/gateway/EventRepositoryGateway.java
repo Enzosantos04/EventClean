f84e7a088c3ebadd0e6afe8685c915b7f272d759
enzosdev <enzohfs@gmail.com>
2025-11-26 16:56:49 -0300

    feat: add EventRepository and EventEntityMapper to have a business rule to create events

--- src/main/java/enzosdev/eventclean/infrastructure/gateway/EventRepositoryGateway.java
+++ src/main/java/enzosdev/eventclean/infrastructure/gateway/EventRepositoryGateway.java
@@ -2,7 +2,8 @@ package enzosdev.eventclean.infrastructure.gateway;
 
 import enzosdev.eventclean.core.entities.Event;
 import enzosdev.eventclean.core.gateway.EventGateway;
-import enzosdev.eventclean.infrastructure.mapper.EventMapper;
+import enzosdev.eventclean.infrastructure.mapper.EventEntityMapper;
+import enzosdev.eventclean.infrastructure.persistence.EventEntity;
 import enzosdev.eventclean.infrastructure.persistence.EventRepository;
 import org.springframework.stereotype.Component;
 
@@ -10,13 +11,20 @@ import org.springframework.stereotype.Component;
 public class EventRepositoryGateway implements EventGateway {
 
     private final EventRepository eventRepository;
+    private final EventEntityMapper eventEntityMapper;
 
-    public EventRepositoryGateway(EventRepository eventRepository) {
+    public EventRepositoryGateway(EventRepository eventRepository, EventEntityMapper eventEntityMapper) {
         this.eventRepository = eventRepository;
+        this.eventEntityMapper = eventEntityMapper;
     }
 
     @Override
     public Event createEvent(Event event) {
-        return eventRepository.save(event);
+        //coverte Event para um EventEntity usando o mapper
+        EventEntity entity = eventEntityMapper.toEntity(event);
+        //salva usando instancia do EventRepository
+        EventEntity newEvent = eventRepository.save(entity);
+        //converte de volta  para Event
+        return eventEntityMapper.toDomain(newEvent);
     }
 }
