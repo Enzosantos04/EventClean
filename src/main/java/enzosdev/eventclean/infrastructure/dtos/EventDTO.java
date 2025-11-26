7736aa172db02fe778ea138e5030dc6e68cbb8c8
enzosdev <enzohfs@gmail.com>
2025-11-26 16:11:25 -0300

    refactor: change the attributes Date to LocalDateTime

--- src/main/java/enzosdev/eventclean/infrastructure/dtos/EventDTO.java
+++ src/main/java/enzosdev/eventclean/infrastructure/dtos/EventDTO.java
@@ -1,13 +1,15 @@
 package enzosdev.eventclean.infrastructure.dtos;
 
 import enzosdev.eventclean.core.enums.EventType;
-import java.util.Date;
+
+import java.time.LocalDateTime;
+
 
 
 public record EventDTO(
         Long id, String name,
         String description, String ticketIdentifier,
-        Date beginAt, Date endAt,
+        LocalDateTime beginAt, LocalDateTime endAt,
         Integer capacity, String location, String eventOrganizer,
         EventType type
 ){}
