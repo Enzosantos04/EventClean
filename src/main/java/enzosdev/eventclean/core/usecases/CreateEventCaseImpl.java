56f5e850e4141c820db95fd1335d0a68e7b065e2
enzosdev <enzohfs@gmail.com>
2025-11-26 16:54:53 -0300

    feat: injecting dependency EventGateway

--- src/main/java/enzosdev/eventclean/core/usecases/CreateEventCaseImpl.java
+++ src/main/java/enzosdev/eventclean/core/usecases/CreateEventCaseImpl.java
@@ -1,8 +1,17 @@
 package enzosdev.eventclean.core.usecases;
 
 import enzosdev.eventclean.core.entities.Event;
+import enzosdev.eventclean.core.gateway.EventGateway;
+
+
 
 public class CreateEventCaseImpl implements CreateEventCase{
+
+
+private final EventGateway eventGateway;
+    public CreateEventCaseImpl(EventGateway eventGateway) {
+        this.eventGateway = eventGateway;
+    }
 
     @Override
     public Event execute(Event event) {
