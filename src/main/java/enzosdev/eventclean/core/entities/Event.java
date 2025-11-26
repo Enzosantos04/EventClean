6cdd3b2060443fe7a8e4fa5a219d0b66f7071354
enzosdev <enzohfs@gmail.com>
2025-11-26 16:10:37 -0300

    refactor: chage the attributes date to LocalDateTime

--- src/main/java/enzosdev/eventclean/core/entities/Event.java
+++ src/main/java/enzosdev/eventclean/core/entities/Event.java
@@ -2,11 +2,12 @@ package enzosdev.eventclean.core.entities;
 
 import enzosdev.eventclean.core.enums.EventType;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public record Event(Long id, String name,
                     String description, String ticketIdentifier,
-                    Date beginAt, Date endAt,
+                    LocalDateTime beginAt, LocalDateTime endAt,
                     Integer capacity, String location, String eventOrganizer,
                     EventType type) {
 
