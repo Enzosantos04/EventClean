82e5d6fb840dbe3c421e3f4db6d8c2d66c0e5ab1
enzosdev <enzohfs@gmail.com>
2025-12-01 15:57:50 -0300

    feat: add findEvents method to EventGateway

--- src/main/java/enzosdev/eventclean/core/gateway/EventGateway.java
+++ src/main/java/enzosdev/eventclean/core/gateway/EventGateway.java
@@ -2,7 +2,11 @@ package enzosdev.eventclean.core.gateway;
 
 import enzosdev.eventclean.core.entities.Event;
 
+import java.util.List;
+
 public interface EventGateway {
 
     Event createEvent(Event event);
+
+    List<Event> findEvents(Event event);
 }
