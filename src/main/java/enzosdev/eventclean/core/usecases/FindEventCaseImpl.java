b8da571300350752ab5435b8388dd2a5cd422886
enzosdev <enzohfs@gmail.com>
2025-12-01 15:56:43 -0300

    feat: class that implements the FindEventCase

--- src/main/java/enzosdev/eventclean/core/usecases/FindEventCaseImpl.java
+++ src/main/java/enzosdev/eventclean/core/usecases/FindEventCaseImpl.java
@@ -1,11 +1,21 @@
 package enzosdev.eventclean.core.usecases;
 
 import enzosdev.eventclean.core.entities.Event;
+import enzosdev.eventclean.core.gateway.EventGateway;
+
+import java.util.List;
 
 public class FindEventCaseImpl implements FindEventCase {
 
+
+    private final EventGateway eventGateway;
+
+    public FindEventCaseImpl(EventGateway eventGateway) {
+        this.eventGateway = eventGateway;
+    }
+
     @Override
-    public Event execute(Event event) {
-        return null;
+    public List<Event> execute(Event event) {
+        return eventGateway.findEvents(event);
     }
 }
