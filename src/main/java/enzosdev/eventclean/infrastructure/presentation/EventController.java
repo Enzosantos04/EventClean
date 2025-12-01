2b014ceae1ec54a34c0c1ced2d9b557de68e9c43
enzosdev <enzohfs@gmail.com>
2025-12-01 16:03:20 -0300

    Add a eventList endpoint on EventController

--- src/main/java/enzosdev/eventclean/infrastructure/presentation/EventController.java
+++ src/main/java/enzosdev/eventclean/infrastructure/presentation/EventController.java
@@ -2,9 +2,12 @@ package enzosdev.eventclean.infrastructure.presentation;
 
 import enzosdev.eventclean.core.entities.Event;
 import enzosdev.eventclean.core.usecases.CreateEventCase;
+import enzosdev.eventclean.core.usecases.FindEventCase;
 import enzosdev.eventclean.infrastructure.dtos.EventDTO;
 import enzosdev.eventclean.infrastructure.mapper.EventMapper;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1")
@@ -12,10 +15,12 @@ public class EventController {
 
     private final CreateEventCase createEventCase;
     private final EventMapper eventMapper;
+    private final FindEventCase findEventCase;;
 
-    public EventController(CreateEventCase createEventCase, EventMapper eventMapper) {
+    public EventController(CreateEventCase createEventCase, EventMapper eventMapper, FindEventCase findEventCase) {
         this.createEventCase = createEventCase;
         this.eventMapper = eventMapper;
+        this.findEventCase = findEventCase;
     }
 
     @PostMapping("createEvent")
@@ -29,8 +34,9 @@ public class EventController {
     }
 
 
-    @GetMapping
-    public String eventList(){
-        return "Lista de eventos";
+    @GetMapping("list")
+    public List<EventDTO> eventList(EventDTO eventDTO){
+        List<Event> eventDTOList =  findEventCase.execute(eventMapper.toEntity(eventDTO));
+        return eventMapper.toDtoList(eventDTOList);
     }
 }
