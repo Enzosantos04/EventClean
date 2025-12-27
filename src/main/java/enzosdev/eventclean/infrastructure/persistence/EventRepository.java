5a271218e6dca704bae588149f8243735ccb0d56
enzosdev <enzohfs@gmail.com>
2025-12-27 18:57:27 -0300

    feat: derived query to findByTicketIdentifier using spring data jpa

--- src/main/java/enzosdev/eventclean/infrastructure/persistence/EventRepository.java
+++ src/main/java/enzosdev/eventclean/infrastructure/persistence/EventRepository.java
@@ -1,6 +1,11 @@
 package enzosdev.eventclean.infrastructure.persistence;
 
+import enzosdev.eventclean.core.entities.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EventRepository extends JpaRepository<EventEntity, Long> {
+
+    Optional<Event> findByticketIdentifier(String identifier);
 }
