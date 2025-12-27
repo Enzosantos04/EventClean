56e81aae697a5127b48b50b0fb02047957691d0e
enzosdev <enzohfs@gmail.com>
2025-12-27 18:46:07 -0300

    feat: implementation of FilterEventIdentifierUsecase

--- src/main/java/enzosdev/eventclean/core/usecases/FilterEventIdentifierUsecaseImpl.java
+++ src/main/java/enzosdev/eventclean/core/usecases/FilterEventIdentifierUsecaseImpl.java
@@ -1,5 +1,20 @@
 package enzosdev.eventclean.core.usecases;
 
-public class FilterEventIdentifierUsecaseImp implements FilterEventIdentifierUsecase{
-    
+import enzosdev.eventclean.core.entities.Event;
+import enzosdev.eventclean.core.gateway.EventGateway;
+import enzosdev.eventclean.infrastructure.exceptions.NotFoundEventException;
+
+public class FilterEventIdentifierUsecaseImpl implements FilterEventIdentifierUsecase{
+
+    private final EventGateway eventGateway;
+
+    public FilterEventIdentifierUsecaseImpl(EventGateway eventGateway) {
+        this.eventGateway = eventGateway;
+    }
+
+    @Override
+    public Event execute(String identifier) {
+        return eventGateway.filterEventByIdentifier(identifier)
+                .orElseThrow(() -> new NotFoundEventException("Event of identifier " + identifier + " not found."));
+    }
 }
