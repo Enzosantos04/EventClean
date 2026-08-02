588b65a7fd389ca4bc52af171c0c1eb9f6252b25
enzosdev <101185305+Enzosantos04@users.noreply.github.com>
2026-08-01 21:30:07 -0300

    feat: add DeleteUsecase implementation overriding execute method

--- src/main/java/enzosdev/eventclean/core/usecases/DeleteEventUsecaseImpl.java
+++ src/main/java/enzosdev/eventclean/core/usecases/DeleteEventUsecaseImpl.java
@@ -1,4 +1,19 @@
 package enzosdev.eventclean.core.usecases;
 
-public class DeleteEventUsecaseImpl {
+
+import enzosdev.eventclean.core.gateway.EventGateway;
+import enzosdev.eventclean.infrastructure.exceptions.NotFoundEventException;
+
+public class DeleteEventUsecaseImpl implements DeleteEventUsecase {
+
+    private EventGateway eventGateway;
+
+    public DeleteEventUsecaseImpl(EventGateway eventGateway) {
+        this.eventGateway = eventGateway;
+    }
+
+    @Override
+    public void execute(String identifier) {
+         eventGateway.deleteEventByIdentifier(identifier);
+    }
 }
